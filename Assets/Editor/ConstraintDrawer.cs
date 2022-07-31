7ce14ce3b03aa64acc171f8c7978d2e1b9b1b246
Lavennie <42303947+Lavennie@users.noreply.github.com>
2022-08-01 00:14:15 +0200

    Create project, IN PROGRESS: 2 chain on x axis with constraints

---
+++ Assets/Editor/ConstraintDrawer.cs
@@ -0,0 +1,22 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(Constraint))]
+public class ConstraintDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        float temp = EditorGUIUtility.labelWidth;
+        EditorGUIUtility.labelWidth = 25.0f;
+
+        EditorGUI.LabelField(new Rect(position.x, position.y, position.width / 3, position.height), label);
+        EditorGUI.PropertyField(new Rect(position.x + position.width / 3, position.y, position.width / 3, position.height), property.FindPropertyRelative("min"));
+        EditorGUI.PropertyField(new Rect(position.x + position.width * 2 / 3, position.y, position.width / 3, position.height), property.FindPropertyRelative("max"));
+
+        EditorGUIUtility.labelWidth = temp;
+
+        EditorGUI.EndProperty();
+    }
+}
